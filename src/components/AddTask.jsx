33e7daef929be82e60dffb10f7b58f03a94b9257
Adithyan Anil <adithyananil2002@gmail.com>
2023-10-29 17:15:52 +0530

    newtaskadd

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function AddTask() {
+export default function AddTask({ task,setTask,AddTasks }) {
+
+
   return (
     <>
       <form className=" w-[40rem] px-[10px] py-[2px]  bg-white  flex place-content-between items-center rounded-[5rem]">
-  
         <input
           type="text"
           className="   rounded-[5rem] w-[32.5rem] px-[1rem] h-[4rem] border-0 text-gray-900 ring-0 outline-none  placeholder:text-gray-400  text-2xl  font-[400] tracking-wide"
           placeholder=" New Task"
+          value={task || ""}
+          onChange={(e) => setTask(e.target.value)}
         />
-        <button className="btn rounded-[5rem] bg-red-600  p-[1rem] flex place-content-center items-center  w-[3.5rem] h-[3.5rem] ">
-          <i class="fa-regular fa-plus fa-xl" style={{ color: "6695e5" }}></i>
+        <button
+          onClick={(e) => AddTasks(e)}
+          className="btn rounded-[5rem] bg-red-600  p-[1rem] flex place-content-center items-center  w-[3.5rem] h-[3.5rem] "
+        >
+          <i className="fa-regular fa-plus fa-xl" style={{ color: "6695e5" }}></i>
         </button>
       </form>
     </>
